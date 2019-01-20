fa53b7e513e738a2325c8197ae63869053b2122a
Irene de Mas <irenedemasc@gmail.com>
2019-01-20 13:08:29 +0100

    minor content fixes

--- src/components/About.js
+++ src/components/About.js
@@ -15,13 +15,12 @@ const About = () => (
         <h2>🚀 Work Hard</h2>
         <p>As soon as I finish a project, I start creating something new — an app, a feature, an article, you name it. <a href="http://bit.ly/happierapps" target="_blank">A deep respect for well-designed products</a> brought me to <a href="https://medium.com/ironhack/coding-is-cheaper-than-therapy-a9448e43e54a" target="_blank">web and iOS development</a>, UX Research, UI Design...
         until I ended up building my own things.
-        My most challenging project so far has been the web app <a href="https://irenedemas.com/#guides">Ask Irene</a>,
+        The most challenging project so far has been the web app <a href="https://irenedemas.com/#guides">Ask Irene</a>,
         where I put into practice all I know (and keep learning) about Product Design.</p>
 
         <h2>🤗 Be Nice to People</h2>
-        <p>Communication is a huge part of my life. I have always focused on connecting with people — be it through design, writing, filmmaking, or anything I can come up with.</p>
-
-        <p>Want to know more about this philosophy? I published an essay on <a href="https://medium.com/@irenedemas/strong-to-be-kind-5c7514b5cde2" target="_blank">self-improvement, working hard and being nice to people</a>.</p>
+        <p>I have always focused on connecting with people — be it through design, writing, filmmaking, or anything I can come up with.
+        Want to know more about this philosophy? I published an essay on <a href="https://medium.com/@irenedemas/strong-to-be-kind-5c7514b5cde2" target="_blank">self-improvement, working hard and being nice to people</a>.</p>
         <h2>... and random things that make me happy</h2>
         <p>Hiking with my dog, listening to podcasts on trains, learning untranslatable words,
       building things that brighten up someone's day, and avocados.</p>
