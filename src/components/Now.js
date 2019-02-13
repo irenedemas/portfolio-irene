131d18685165c56279093c1564d3e41f66056d41
Irene de Mas <irenedemasc@gmail.com>
2019-02-13 20:53:22 +0100

    target blank added

--- src/components/Now.js
+++ src/components/Now.js
@@ -6,15 +6,15 @@ const About = () => (
     <div className="row">
       <div className="nine columns main-col">
         <h1>HEAVY FIXATION</h1>
-        <h2>50% heavy rotation playlist, 40% <a href="https://nownownow.com/about">now page</a>, 10% <a href="https://medium.com/ironhack/coding-is-cheaper-than-therapy-a9448e43e54a">OCD</a></h2>
+        <h2>50% heavy rotation playlist, 40% <a href="https://nownownow.com/about" target="_blank">now page</a>, 10% <a href="https://medium.com/ironhack/coding-is-cheaper-than-therapy-a9448e43e54a">OCD</a></h2>
         <h3>Food</h3>
         <p>👵 Restaurants for middle aged people</p>
         <h3>Music</h3>
-        <p>🎸 <a href="http://www.vancejoy.com">Vance Joy</a></p>
+        <p>🎸 <a href="http://www.vancejoy.com" target="_blank">Vance Joy</a></p>
         <h3>Podcast</h3>
-        <p>🏗️ <a href="https://www.relay.fm/radar">Under the Radar</a></p>
+        <p>🏗️ <a href="https://www.relay.fm/radar" target="_blank">Under the Radar</a></p>
         <h3>Film</h3>
-        <p>💑 <a href="https://www.imdb.com/title/tt5096846/">The Boy Downstairs</a></p>
+        <p>💑 <a href="https://www.imdb.com/title/tt5096846/" target="_blank">The Boy Downstairs</a></p>
         <h3>Project</h3>
         <p>⚡️ Pansa (more info coming sometime 2019)</p>
       </div>
