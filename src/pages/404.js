10b6e60888a6815c667d560ec05dbdd3dda4a725
Irene de Mas <irenedemasc@gmail.com>
2019-02-15 11:02:03 +0100

    404 page updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist 😱</p>
-  </div>
-)
+  <section id="fixations">
+    <div className="row">
+      <div className="nine columns main-col">
+        <h1>PAGE NOT FOUND</h1>
+        <h2>Are you lost?</h2>
+        <p>😥 Yes, take me <a href="https://irenedemas.com">home</a></p>
+        <p>🌝 No, I'll stay on 404 limbo</p>
+      </div>
+    </div>
+  </section>
+);
 
 export default NotFoundPage
