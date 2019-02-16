200da1b6a164e29893f37b07d388db893cd48b2f
Irene de Mas <irenedemasc@gmail.com>
2019-02-16 09:05:48 +0100

    podcast fixation updated

--- src/components/Fixations.js
+++ src/components/Fixations.js
@@ -11,7 +11,7 @@ const About = () => (
         <h3>Music</h3>
         <p>🎸 <a href="http://www.vancejoy.com" target="_blank">Vance Joy</a></p>
         <h3>Podcast</h3>
-        <p>🏗️ <a href="https://www.relay.fm/radar" target="_blank">Under the Radar</a></p>
+        <p>🔧 <a href="https://fixedthatforyou.com" target="_blank">Fixed That For You</a></p>
         <h3>Film</h3>
         <p>💑 <a href="https://www.imdb.com/title/tt5096846/" target="_blank">The Boy Downstairs</a></p>
         <h3>Project</h3>
