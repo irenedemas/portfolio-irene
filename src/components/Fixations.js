df82b3571e1406d3ec237641fe43c195c1a78eff
irenedemas <irenedemasc@gmail.com>
2019-04-07 15:37:04 +0200

    updated fixations

--- src/components/Fixations.js
+++ src/components/Fixations.js
@@ -9,7 +9,7 @@ const About = () => (
         <h3>Food</h3>
         <p>👨‍ Restaurants for middle aged people</p>
         <h3>Music</h3>
-        <p>🎸 <a href="http://www.bestcoast.net" target="_blank">California Nights (again)</a></p>
+        <p>🎸 <a href="https://open.spotify.com/track/5e9TFTbltYBg2xThimr0rU?si=HQ-Bi1lXTaq-q7VAtTuTFg" target="_blank">The Chain — Fleetwood Mac</a></p>
         <h3>Podcast</h3>
         <p>🔧 <a href="https://fixedthatforyou.com" target="_blank">Fixed That For You</a></p>
         <h3>Film</h3>
