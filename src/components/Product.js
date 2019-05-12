84aa8eb26c5b3e3fa7925f4b1fd1b38342360112
irenedemas <irenedemasc@gmail.com>
2019-05-12 13:09:45 +0200

    product section content improved

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,13 +5,13 @@ const About = () => (
     <div className="row">
       <div className="nine columns main-col">
         <h1>ASK IRENE</h1>
-        <h2>Ask Irene is a web app that helps you find the perfect spot for any occasion 🍜</h2>
+        <h2>A web app that helps you find the perfect spot for any occasion 🍜</h2>
         <p>I've been obsessed with food, cozy cafés and cool bathrooms for as long as I can remember. Thanks to that, I have a mental database of places and always end up recommending them to friends,
         acquaintances and nosy strangers. On <a href="https://www.irene.cool" target="_blank">Ask Irene</a> you can access my wisdom 24/7
         to find the perfect spot for you. Curious about how we built Ask Irene in just two weeks? <a href="https://medium.com/ironhack/building-ask-irene-841f3d939d00" target="_blank">There's an article for that.</a></p>
         <h1>QWORTY</h1>
-        <h2><a href="https://qworty.co" target="_blank">QWORTY</a> is an iOS keyboard for movie buffs and pop culture freaks 💫</h2>
-        <p>Instantly find thousands of quotes from movies, songs, books, memes, or anywhere you can imagine. Search, edit and share any quote — no matter the app you're using.</p>
+        <h2>An iOS keyboard for movie buffs and pop culture freaks 💫</h2>
+        <p>Instantly find thousands of quotes from movies, songs, books, memes, or anywhere you can imagine. With <a href="https://qworty.co" target="_blank">QWORTY</a> you can search, edit and share any quote — no matter the app you're using.</p>
         <br />
         <h2>Not enough?</h2>
         <p>More info about other projects coming soon.</p>
