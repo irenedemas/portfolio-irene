e5d659028019edf50dee0b0adab84580b2aa6887
irenedemas <irenedemasc@gmail.com>
2019-05-12 12:45:00 +0200

    product section copy updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,12 +9,12 @@ const About = () => (
         <p>I've been obsessed with food, cozy cafés and cool bathrooms for as long as I can remember. Thanks to that, I have a mental database of places and always end up recommending them to friends,
         acquaintances and nosy strangers. On <a href="https://www.irene.cool" target="_blank">Ask Irene</a> you can access my wisdom 24/7
         to find the perfect spot for you. Curious about how we built Ask Irene in just two weeks? <a href="https://medium.com/ironhack/building-ask-irene-841f3d939d00" target="_blank">There's an article for that.</a></p>
-        <a a href="https://www.irene.cool" target="_blank" className="button">ASK IRENE</a>
         <h1>QWORTY</h1>
         <h2><a href="https://qworty.co" target="_blank">QWORTY</a> is an iOS keyboard for movie buffs and pop culture freaks 💫</h2>
         <p>Instantly find thousands of quotes from movies, songs, books, memes, or anywhere you can imagine. Search, edit and share any quote — no matter the app you're using.</p>
-        <a a href="https://qworty.co" target="_blank" className="button">QWORTY</a>
-        <h2>More products coming soon</h2>
+        <br />
+        <h2>Not enough?</h2>
+        <p>More info about other projects coming soon.</p>
       </div>
     </div>
   </section>
