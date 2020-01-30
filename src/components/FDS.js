dd3a18f93f9e6d53debd81e7b82eb417af12a859
irenedemas <irenedemasc@gmail.com>
2020-01-30 18:17:06 +0100

    FDS updated

--- src/components/FDS.js
+++ src/components/FDS.js
@@ -15,9 +15,9 @@ const Work = () => (
         <div className="row item">
             <div className="twelve columns">
                 <h3 className="info">
-                <a href="ttps://www.finnishdesignshop.com/design-stories/travel/barcelona-top-3-restaurants-for-design-lovers" target="_blank">Restaurants for Design Lovers</a>
+                <a href="https://www.finnishdesignshop.com/design-stories/travel/barcelona-top-3-restaurants-for-design-lovers" target="_blank">Restaurants for Design Lovers</a>
                 <span />
-                <em className="date">Barcelona, where great food and interior design meet</em>
+                <em className="date">Where great food and interior design meet</em>
                 </h3>
             </div>
         </div>
