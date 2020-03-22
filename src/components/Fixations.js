352c810034747bbf0f4116f9761705e05460a13d
irenedemas <irenedemasc@gmail.com>
2020-03-22 15:01:20 +0100

    fixations updated

--- src/components/Fixations.js
+++ src/components/Fixations.js
@@ -7,7 +7,7 @@ const About = () => (
         <h1>HEAVY FIXATION</h1>
         <h2>50% heavy rotation playlist, 40% <a href="https://nownownow.com/about" target="_blank">now page</a>, 10% <a href="https://medium.com/ironhack/coding-is-cheaper-than-therapy-a9448e43e54a" target="_blank">OCD</a></h2>
         <h3>Food</h3>
-        <p>🌶️ Kimchi anything</p>
+        <p>🍛 Homemade meatballs</p>
         <h3>Music</h3>
         <p>🎸 <a href="https://open.spotify.com/album/31qVWUdRrlb8thMvts0yYL?si=se-aYPWQTh-VUVTwUWbsIA" target="_blank">The Slow Rush (Tame Impala, 2020)</a></p>
         <h3>Podcast</h3>
@@ -15,7 +15,7 @@ const About = () => (
         <h3>Book</h3>
         <p>🌇 <a href="https://www.goodreads.com/book/show/22711627-building-and-dwelling" target="_blank">Building and Dwelling (Richard Sennett, 2018)</a></p>
         <h3>Film</h3>
-        <p>🏡 <a href="https://www.imdb.com/title/tt6751668/" target="_blank">Parasite (Bong Joon Ho, 2019)</a></p>
+        <p>💐 <a href="https://www.imdb.com/title/tt8772262/" target="_blank">Midsommar (Ari Aster, 2019)</a></p>
         <h3>Project</h3>
         <p>🤖 <a href="https://www.irene.cool" target="_blank">Ask Irene 2.0</a></p>
       </div>
