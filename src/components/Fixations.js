bea4b6ae59fd7b08b1d6d51b5d5fcde088a8125e
irenedemas <irenedemasc@gmail.com>
2020-06-03 13:47:56 +0200

    fixations updated

--- src/components/Fixations.js
+++ src/components/Fixations.js
@@ -7,15 +7,15 @@ const About = () => (
         <h1>HEAVY FIXATION</h1>
         <h2>50% heavy rotation playlist, 40% <a href="https://nownownow.com/about" target="_blank">now page</a>, 10% <a href="https://medium.com/ironhack/coding-is-cheaper-than-therapy-a9448e43e54a" target="_blank">OCD</a></h2>
         <h3>Food</h3>
-        <p>🍛 Homemade meatballs</p>
+        <p>🍌 Frozen chocolate banana bread</p>
         <h3>Music</h3>
-        <p>🎸 <a href="https://open.spotify.com/album/31qVWUdRrlb8thMvts0yYL?si=se-aYPWQTh-VUVTwUWbsIA" target="_blank">The Slow Rush (Tame Impala, 2020)</a></p>
+        <p>🦢 <a href="https://open.spotify.com/album/7bvmGyFDwpHNRRRZJ0AHvn?si=dBlLm209Q12F3fnAJtAaqQ" target="_blank">Negro Swan (Blood Orange, 2018)</a></p>
         <h3>Podcast</h3>
-        <p>👨‍💻 <a href="http://replyall.limo" target="_blank">Reply All (Alex Goldman & PJ Vogt)</a></p>
+        <p>😱 <a href="https://gimletmedia.com/shows/scaredy-cats" target="_blank">The Scaredy Cats Horror Show (Alex Goldman & PJ Vogt)</a></p>
         <h3>Book</h3>
-        <p>🌇 <a href="https://www.goodreads.com/book/show/22711627-building-and-dwelling" target="_blank">Building and Dwelling (Richard Sennett, 2018)</a></p>
+        <p>⚗️ <a href="https://de.phaidon.com/store/food-cook/rene-redzepi-a-work-in-progress-9780714866918/" target="_blank">A Work in Progress (René Redzepi, 2013)</a></p>
         <h3>Film</h3>
-        <p>💐 <a href="https://www.imdb.com/title/tt8772262/" target="_blank">Midsommar (Ari Aster, 2019)</a></p>
+        <p>🐻 <a href="https://www.netflix.com/title/80049832" target="_blank">Our Planet (BBC, 2019)</a></p>
         <h3>Project</h3>
         <p>🤖 <a href="https://www.irene.cool" target="_blank">Ask Irene 2.0</a></p>
       </div>
