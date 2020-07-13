9de9844718610311165484a0bde823b707653005
irenedemas <irenedemasc@gmail.com>
2020-07-13 14:41:01 +0200

    fixations updated

--- src/components/Fixations.js
+++ src/components/Fixations.js
@@ -7,13 +7,13 @@ const About = () => (
         <h1>HEAVY FIXATION</h1>
         <h2>50% heavy rotation playlist, 40% <a href="https://nownownow.com/about" target="_blank">now page</a>, 10% <a href="https://medium.com/ironhack/coding-is-cheaper-than-therapy-a9448e43e54a" target="_blank">OCD</a></h2>
         <h3>Food</h3>
-        <p>🍌 Frozen chocolate banana bread</p>
+        <p>🧀 Cheesy Polenta</p>
         <h3>Music</h3>
         <p>🦢 <a href="https://open.spotify.com/album/7bvmGyFDwpHNRRRZJ0AHvn?si=dBlLm209Q12F3fnAJtAaqQ" target="_blank">Negro Swan (Blood Orange, 2018)</a></p>
         <h3>Podcast</h3>
-        <p>😱 <a href="https://gimletmedia.com/shows/scaredy-cats" target="_blank">The Scaredy Cats Horror Show (Alex Goldman & PJ Vogt)</a></p>
+        <p>💀 <a href="https://www.primaverasound.com/en/radio/shows/deforme-semanal-ideal-total" target="_blank">Deforme Semanal Ideal Total (Isa Calderón & Lucía Lijtmaer)</a></p>
         <h3>Book</h3>
-        <p>⚗️ <a href="https://de.phaidon.com/store/food-cook/rene-redzepi-a-work-in-progress-9780714866918/" target="_blank">A Work in Progress (René Redzepi, 2013)</a></p>
+        <p>⚗️ <a href="https://www.goodreads.com/book/show/133954.Washington_Square" target="_blank">Washington Square (Henry James, 1880)</a></p>
         <h3>Film</h3>
         <p>🐻 <a href="https://www.netflix.com/title/80049832" target="_blank">Our Planet (BBC, 2019)</a></p>
         <h3>Project</h3>
