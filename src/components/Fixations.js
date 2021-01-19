315ca12d23d2f29d98cb99df622caac32ec66d01
irenedemas <irenedemasc@gmail.com>
2021-01-19 17:48:35 +0100

    fixations updated

--- src/components/Fixations.js
+++ src/components/Fixations.js
@@ -7,15 +7,15 @@ const About = () => (
         <h1>HEAVY FIXATION</h1>
         <h2>50% heavy rotation playlist, 40% <a href="https://nownownow.com/about" target="_blank">now page</a>, 10% <a href="https://medium.com/ironhack/coding-is-cheaper-than-therapy-a9448e43e54a" target="_blank">OCD</a></h2>
         <h3>Food</h3>
-        <p>🧀 Cheesy polenta</p>
+        <p>🍜 Spicy Soups</p>
         <h3>Music</h3>
-        <p>🦢 <a href="https://open.spotify.com/album/7bvmGyFDwpHNRRRZJ0AHvn?si=dBlLm209Q12F3fnAJtAaqQ" target="_blank">Negro Swan (Blood Orange, 2018)</a></p>
+        <p>🏀 <a href="https://open.spotify.com/artist/0gasUYSkVsXgX3p3bera9b?si=1HNao1X9RY-rgMQ5a1UfQQ" target="_blank">Lildami</a></p>
         <h3>Podcast</h3>
         <p>💀 <a href="https://www.primaverasound.com/en/radio/shows/deforme-semanal-ideal-total" target="_blank">Deforme Semanal Ideal Total (Isa Calderón & Lucía Lijtmaer)</a></p>
         <h3>Book</h3>
-        <p>🎩 <a href="https://www.goodreads.com/book/show/133954.Washington_Square" target="_blank">Washington Square (Henry James, 1880)</a></p>
+        <p>🎩 <a href="https://www.goodreads.com/book/show/53835.The_Age_of_Innocence" target="_blank">The Age of Innocence (Edith Wharton, 1920)</a></p>
         <h3>Film</h3>
-        <p>🐻 <a href="https://www.netflix.com/title/80049832" target="_blank">Our Planet (BBC, 2019)</a></p>
+        <p>🏰 <a href="https://www.netflix.com/title/80049832" target="_blank">Borgen (DR1, 2010)</a></p>
         <h3>Project</h3>
         <p>🤖 <a href="https://www.irene.cool" target="_blank">Ask Irene 2.0</a></p>
       </div>
